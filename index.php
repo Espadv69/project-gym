b9c5808ba403c56cdf65dcf83f65fc2237a3ff4c
aitor <aitorjob69@gmail.com>
2024-05-14 12:48:34 +0200

    new title

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if(isset($_SESSION['username'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>GYM BRO'S</title>
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body class="body-reg-log">
