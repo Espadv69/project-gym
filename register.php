b71fee04f30749c6df3ae604a2e7ea08fb01da01
aitor <aitorjob69@gmail.com>
2024-05-14 12:48:47 +0200

    lang ES

--- register.php
+++ register.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['username'])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
