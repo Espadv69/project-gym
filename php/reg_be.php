36e27a09b8b3fe7d30025e91407e972e44163602
aitor <aitorjob69@gmail.com>
2024-05-14 12:04:47 +0200

    check user and email

--- php/reg_be.php
+++ php/reg_be.php
@@ -1,6 +1,7 @@
 <?php 
 require_once "./conn.php";
 
+// VARIABLES
 $name = $_POST['name'];
 $username = $_POST['username'];
 $email = $_POST['email'];
@@ -17,7 +18,44 @@ $query = "INSERT INTO login(name, username, email, pass1, pass2)VALUES(
     '$pass2'
 )";
 
+// CHECK USERNAME
+$check_username = mysqli_query($conn, "SELECT * FROM login WHERE username='$username'");
 
+if(mysqli_num_rows($check_username) > 0) {
+    echo "
+    <script>
+        alert('ESTE USUARIO YA ESTÁ EN USO ❌');
+        window.location = '../register.php';
+    </script>
+    ";
+    exit;
+}
+
+// CHECK EMAIL
+$check_email = mysqli_query($conn, "SELECT * FROM login WHERE email='$email'");
+
+if(mysqli_num_rows($check_email) > 0) {
+    echo "
+    <script>
+        alert('ESTE EMAIL YA ESTÁ EN USO ❌');
+        window.location = '../register.php';
+    </script>
+    ";
+    exit;
+}
+
+// CHECK PASSWORDS
+if($pass1 !== $pass2) {
+    echo "
+    <script>
+        alert('LAS CONTRASEÑAS NO COINCIDEN ❌');
+        window.location = '../register.php';
+    </script>
+    ";
+    exit;
+}
+
+// INSERT INTO TABLE
 $insert = mysqli_query($conn, $query);
 
 if($query) {
@@ -28,3 +66,5 @@ if($query) {
     </script>
     ";
 }
+
+mysqli_close($conn);
