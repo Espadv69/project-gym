e1e7bfb857cae6106e64923f1b3d87a14bda0902
aitor <aitorjob69@gmail.com>
2024-05-14 13:57:16 +0200

    created new page

---
+++ rutina.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link
+    rel="stylesheet"
+    href="./css/style.css">
+
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous"
+    />
+
+</head>
+<body>
+    <nav
+        class="nav justify-content-center flex-column"
+    >
+        <a class="nav-link active" href="./rutina.php" aria-current="page">
+            Active link
+        </a>
+
+        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Disabled link</a>
+        <a href="./php/close_conn.php">CERRAR SESIÓN</a>
+    </nav>
+</body>
+</html>
