24c33f5a7ab25487cf823678c74549c277a7a6f2
aitor <aitorjob69@gmail.com>
2024-05-14 10:21:37 +0200

    body class

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
-<body>
-    <p>Si aún no te has registrado<a href="register.php">pinche aquí</a></p>
+<body class="body-reg-log">
+    <p>Si aún no te has registrado <a href="register.php">pinche aquí</a></p>
 </body>
 </html>
