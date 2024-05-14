0b3dcd39b1f5329530ca6e4f0faed36037c47662
aitor <aitorjob69@gmail.com>
2024-05-14 13:57:07 +0200

    link bs5 and aside

--- home.php
+++ home.php
@@ -17,9 +17,41 @@ if(!isset($_SESSION['username'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="./css/style.css">
+
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous"
+    />
+
+    <link
+        rel="stylesheet"
+        href="./css/style.css"
+    />
+
 </head>
 <body class="body-home">
-    <a href="./php/close_conn.php">CERRAR SESIÓN</a>
+
+    <header>
+        <img src="./img/boxer.jpg" alt="boxeadora">
+        <h2>GYM BROTHER'S</h2>
+    </header>
+
+    <nav
+        class="nav justify-content-center flex-column"
+    >
+        <a class="nav-link active" href="./rutina.php" aria-current="page">
+            Active link
+        </a>
+
+        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Disabled link</a>
+        <a href="./php/close_conn.php">CERRAR SESIÓN</a>
+    </nav>
+        
+
+
+
 </body>
 </html>
