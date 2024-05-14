63b4faf4e22f634428b72a03bbc9596b47dd7a2e
aitor <aitorjob69@gmail.com>
2024-05-14 09:52:46 +0200

    form reg

--- register.php
+++ register.php
@@ -6,6 +6,22 @@
     <title>Register</title>
 </head>
 <body>
-    
+    <main>
+
+    <form action="./php/reg_be.php" method="post" name="form-reg">
+
+        <input type="text" name="name" placeholder="Ingrese su nombre">
+
+        <input type="text" name="username" placeholder="Crea tu usuario">
+
+        <input type="email" name="email" placeholder="nombre@ejemplo.com">
+
+        <input type="password" name="pass1" placeholder="Ingrese contraseña...">
+
+        <input type="password" name="pass2" placeholder="Repita contraseña...">
+
+    </form>
+
+    </main>
 </body>
 </html>
