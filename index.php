3a14ff0eb78bb2f2f0c6b2dc5e0e65b73ab6044c
aitor <aitorjob69@gmail.com>
2024-05-14 09:52:38 +0200

    <a> to register

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
 <body>
-    
+    <p>Si aún no te has registrado<a href="register.php">pinche aquí</a></p>
 </body>
 </html>
