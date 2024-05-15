2f63e56a770140cc52ce5b23cb1e55848429b0ef
aitor <aitorjob69@gmail.com>
2024-05-15 12:04:08 +0200

    new file rutina created

--- rutina.php
+++ rutina.php
@@ -1,33 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Rutina</title>
 
     <link
-    rel="stylesheet"
-    href="./css/style.css">
-
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
         rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-        crossorigin="anonymous"
-    />
+        href="./css/style.css"
+    >
 
 </head>
-<body>
-    <nav
-        class="nav justify-content-center flex-column"
-    >
-        <a class="nav-link active" href="./rutina.php" aria-current="page">
-            Active link
-        </a>
+    <body>
 
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link" href="#">Disabled link</a>
-        <a href="./php/close_conn.php">CERRAR SESIÓN</a>
-    </nav>
-</body>
+    </body>
 </html>
