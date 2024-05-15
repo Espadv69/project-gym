64d46622af08b8e0029e4612c3b32f6c178f8699
aitor <aitorjob69@gmail.com>
2024-05-15 12:04:57 +0200

    new file footer with footer

---
+++ template/footer.php
@@ -0,0 +1,25 @@
+<link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+    crossorigin="anonymous"
+/>
+
+<link
+    rel="stylesheet"
+    href="../css/style.css"
+>
+
+<link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+>
+
+<footer>
+
+    <a href="https://github.com/"><i class="bi bi-github"></i></a>
+    <a href="https://twitter.com/?lang=es"><i class="bi bi-twitter-x"></i></a>
+    <a href="https://www.facebook.com/?locale=es_ES"><i class="bi bi-facebook"></i></a>
+    <a href="https://www.instagram.com/"><i class="bi bi-instagram"></i></a>
+
+</footer>
