4e3716ca5000296107abb97410d43327ca2dbdc3
aitor <aitorjob69@gmail.com>
2024-05-15 12:05:15 +0200

    new file navbar with navbar

---
+++ template/navbar.php
@@ -0,0 +1,31 @@
+<link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous"
+/>
+
+<link
+    rel="stylesheet"
+    href="../css/style.css"
+>
+
+<nav
+    class="navbar navbar-expand-sm navbar-light bg-light"
+>
+    <div class="container">
+
+        <a class="a" href="#">Home🏠</a>
+
+        <a class="a" href="./rutina.php">Rutina☀️</a>
+
+        <a class="a" href="./nutricion.php">Nutrición🍎</a>
+
+        <a class="a" href="#">Calcular IMC⚖️</a>
+
+        <a class="a" href="#">Contacto📱</a>
+
+        <a class="a" href="./php/close_conn.php">Cerrar sesión❌</a>
+
+    </div>
+</nav>
