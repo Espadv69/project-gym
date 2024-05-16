020eb837a84fc40c17f9a49b9ee4bb656d48fbc8
aitor <aitorjob69@gmail.com>
2024-05-16 09:20:47 +0200

    html open

--- imc.php
+++ imc.php
@@ -9,3 +9,20 @@ if(!isset($_SESSION['username'])) {
     ";
 }
 ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rutina</title>
+
+    <link
+        rel="stylesheet"
+        href="./css/style.css"
+    >
+
+</head>
+    <body>
+
+    </body>
+</html>
