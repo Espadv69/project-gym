fb284fcbf609ef0114ba974722de75cdfdabf2de
aitor <aitorjob69@gmail.com>
2024-05-16 10:41:49 +0200

    space fixed

--- nutricion.php
+++ nutricion.php
@@ -11,17 +11,17 @@ if(!isset($_SESSION['username'])) {
 ?>
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rutina</title>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Rutina</title>
 
-    <link
-        rel="stylesheet"
-        href="./css/style.css"
-    >
+        <link
+            rel="stylesheet"
+            href="./css/style.css"
+        >
 
-</head>
+    </head>
     <body>
 
     </body>
