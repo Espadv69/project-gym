8b3002a4e4cc97680f6d0ccc7dce970e6364047c
aitor <aitorjob69@gmail.com>
2024-05-17 12:41:02 +0200

    p with an ID

--- imc.php
+++ imc.php
@@ -35,6 +35,8 @@ if(!isset($_SESSION['username'])) {
             
             <input type="button" value="Calcular" class="boton">
 
+            <p id="imcP"></p>
+
         </section>
 
         <?php require_once "./template/footer.php"?>
